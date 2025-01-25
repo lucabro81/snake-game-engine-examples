fd9588599f01026888534b1b194a013c872eb598
lucabro81 <luca.brognara81@gmail.com>
2025-01-25 16:50:25 +0100

    feat: basic websocket comunication

--- apps/vite-multiplayer/src/main.ts
+++ apps/vite-multiplayer/src/main.ts
@@ -1,24 +1,28 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+      <button id="counter" type="button">test</button>
+      <button id="connect" type="button">connect</button>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let ws: WebSocket;
+
+function connect() {
+  ws = new WebSocket("http://localhost:8080");
+  console.log("connected", ws);
+  ws.onmessage = (event) => {
+    console.log("event", event.data);
+  };
+}
+
+function sendMessage() {
+  ws.send("hello");
+}
+
+document.querySelector<HTMLButtonElement>('#counter')!
+  .addEventListener('click', sendMessage)
+
+document.querySelector<HTMLButtonElement>('#connect')!
+  .addEventListener('click', connect)
